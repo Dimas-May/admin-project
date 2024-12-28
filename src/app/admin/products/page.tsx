9d04166e847a1b0fcc83e2ba875a8a4228ceb091
Dimas-May <cardo24bekasi@gmail.com>
2024-12-28 22:21:11 +0700

    commit4

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="h-calc(100vh-96px) w-full grid place-items-center overflow-y-auto">
+      <div className="bg-white w-[300px] rounded-lg p-4 "></div>
+    </div>
+  );
+};
+
+export default Products;
