824af090534588850818687231fe7c1e006dd0eb
Dimas-May <cardo24bekasi@gmail.com>
2024-12-29 00:14:12 +0700

    commit5

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,9 +1,12 @@
+import ProductForm from "@/components/admin-panel/ProductForm";
 import React from "react";
 
 const Products = () => {
   return (
-    <div className="h-calc(100vh-96px) w-full grid place-items-center overflow-y-auto">
-      <div className="bg-white w-[300px] rounded-lg p-4 "></div>
+    <div className="h-[calc(100vh-96px)] w-full grid place-items-center overflow-y-auto">
+      <div className="bg-white w-[300px] rounded-lg p-4 ">
+        <ProductForm />
+      </div>
     </div>
   );
 };
