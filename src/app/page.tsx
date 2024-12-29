0f0e669e1123f1012237029a57fbbeeea5034be1
Dimas-May <cardo24bekasi@gmail.com>
2024-12-30 04:32:33 +0700

    commit8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 "use client";
+import Banner from "@/components/front-end/Banner";
+import Cart from "@/components/front-end/Cart";
+import Feature from "@/components/front-end/Feature";
+import Hero from "@/components/front-end/Hero";
 import Navbar from "@/components/front-end/Navbar";
+import TrendingProducts from "@/components/front-end/TrendingProducts";
 import { useState } from "react";
 
 export default function Home() {
@@ -8,6 +13,11 @@ export default function Home() {
   return (
     <main>
       <Navbar setShowCart={setShowCart} />
+      {showCart && <Cart setShowCart={setShowCart} />}
+      <Hero />
+      <Feature />
+      <TrendingProducts />
+      <Banner />
     </main>
   );
 }
